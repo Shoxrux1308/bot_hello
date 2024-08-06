b1c308bb04dc90b6faec08bba72d5d596b55e61c
Shoxrux1308 <shohruxb1308@gmail.com>
2024-08-06 11:14:17 +0500

    text_next

--- get_updates.py
+++ get_updates.py
@@ -2,7 +2,7 @@ import requests
 import os
 TOKEN = os.environ['TOKEN'] 
 URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
-
+import time
 
 def get_text_from_update(update):
     """
@@ -14,10 +14,16 @@ def get_text_from_update(update):
     Returns:
         str: text from update
     """
-    return update["result"][-1]["message"]["text"]
+    return update["message"]["text"]
 
 # Get updates
-response = requests.get(URL)
-data = response.json()
-print(get_text_from_update(data))
-
+first_update_id=-1
+while True:
+    response = requests.get(URL)
+    data = response.json()
+    result=data['result'][-1]
+    next_update_id=result
+    if first_update_id!=next_update_id["update_id"]:
+        print(get_text_from_update(result))
+    first_update_id=next_update_id["update_id"]
+    
